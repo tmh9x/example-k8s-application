5285cd0c18fc03c9f80e98e7cfe2d989723858c2
Marco Lindner <marco.lindner@gmx.org>
2024-04-24 20:45:05 +0200

    feat(app): adding envs for dev & prod

--- frontend/src/app/systeminformation.service.ts
+++ frontend/src/app/systeminformation.service.ts
@@ -1,23 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SysteminformationService {
+  apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   getSystem() {
-    return this.http.get('http://localhost:3000/systeminformation/system');
+    return this.http.get(`${this.apiUrl}/systeminformation/system`);
   }
   getCpu() {
-    return this.http.get('http://localhost:3000/systeminformation/cpu');
+    return this.http.get(`${this.apiUrl}/systeminformation/cpu`);
   }
   getMem() {
-    return this.http.get('http://localhost:3000/systeminformation/memory');
+    return this.http.get(`${this.apiUrl}/systeminformation/memory`);
   }
   getOs() {
-    return this.http.get('http://localhost:3000/systeminformation/os');
+    return this.http.get(`${this.apiUrl}/systeminformation/os`);
   }
 }
